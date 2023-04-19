38e5665ec88a957d336487219fcfdd1da1f28e4f
DiegooFuentes <diegofuentesm123@gmail.com>
2023-04-18 23:10:04 -0400

    Exercise 3

---
+++ src/main/java/introduction_to_java/ArithmeticOperations2.java
@@ -0,0 +1,9 @@
+package introduction_to_java;
+//Exercise 3: Displaying Non-ASCII Characters
+public class ArithmeticOperations2 {
+    public static void main(String[] args) {
+        System.out.println("Non-ASCII characters: ☺ ");
+        System.out.println("∀x ∈ ℝ: ⌈x⌉ = −⌊−x⌋");
+        System.out.println("π ≅ " + 3.1415926535 ); // + is used to concatenate
+    }
+}
