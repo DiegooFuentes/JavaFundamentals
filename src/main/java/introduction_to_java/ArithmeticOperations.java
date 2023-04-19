e71ebe9c5dd13d07e26325a83903a2a4fb894716
DiegooFuentes <diegofuentesm123@gmail.com>
2023-04-18 23:03:09 -0400

    Exercise 2

--- src/main/java/introduction_to_java/ArithmeticOperations.java
+++ src/main/java/introduction_to_java/ArithmeticOperations.java
@@ -4,7 +4,7 @@ public class ArithmeticOperations {
     public static void main(String[] args) {
         System.out.println(4 + 5);
         System.out.println(4 * 5);
-        System.out.println(4 / 5);
+        System.out.println(4.0 / 5); //Using floating point to display the decimal result
         System.out.println(9 / 2);
     }
 }
