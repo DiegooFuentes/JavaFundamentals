4ed750658103990ff8113f1c4035fc5e6fcfdda8
DiegooFuentes <diegofuentesm123@gmail.com>
2023-04-18 23:13:06 -0400

    Activity 1

---
+++ src/main/java/introduction_to_java/Activity1.java
@@ -0,0 +1,8 @@
+package introduction_to_java;
+
+public class Activity1 {
+    public static void main(String[] args) {
+        System.out.println("The sum of 3 + 4 is " + (3 + 4));
+        System.out.println("The product of 3 + 4 is " + (3 * 4));
+    }
+}
