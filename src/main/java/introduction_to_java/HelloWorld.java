c1841122cf31b31bef74b387f4e0b9b07296593e
DiegooFuentes <diegofuentesm123@gmail.com>
2023-04-18 22:55:56 -0400

    Exercise 1

---
+++ src/main/java/introduction_to_java/HelloWorld.java
@@ -0,0 +1,7 @@
+package introduction_to_java;
+//Exercise 1: A simple Hello World Program
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
