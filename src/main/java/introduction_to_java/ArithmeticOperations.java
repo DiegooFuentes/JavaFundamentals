23d1caa2a477fc053a120132003de0cc85416987
DiegooFuentes <diegofuentesm123@gmail.com>
2023-04-18 23:00:43 -0400

    Exercise 2

---
+++ src/main/java/introduction_to_java/ArithmeticOperations.java
@@ -0,0 +1,10 @@
+package introduction_to_java;
+//Exercise 2: A Simple Program for Performing Simple Mathematics Operations
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        System.out.println(4 + 5);
+        System.out.println(4 * 5);
+        System.out.println(4 / 5);
+        System.out.println(9 / 2);
+    }
+}
