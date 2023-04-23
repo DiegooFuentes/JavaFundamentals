8802aa0df7383d08540988a2998e55a3a3fc5dac
DiegooFuentes <diegofuentesm123@gmail.com>
2023-04-22 20:38:35 -0400

    Activity 4

---
+++ src/main/java/variables_dataTypes_andOperators/SimpleIdCard.java
@@ -0,0 +1,24 @@
+package variables_dataTypes_andOperators;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+//Activity 4: Inputting Student Information and Outputting an ID
+public class SimpleIdCard {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("What's your name? ");
+        String name = sc.nextLine();
+        System.out.print("What's your university? ");
+        String university = sc.nextLine();
+        System.out.print("What's your age? ");
+        int age = sc.nextInt();
+
+        System.out.println("Here is your ID \n" +
+                "************************** \n" +
+                "Name: " + name + "\n" +
+                "University: " + university + "\n" +
+                "Age: " + age + "\n" +
+                "**************************");
+    }
+}
