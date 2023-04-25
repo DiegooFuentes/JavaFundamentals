c61fbd6ea09113652ab1cdc95cddd41abce22efa
DiegooFuentes <diegofuentesm123@gmail.com>
2023-04-25 17:48:50 -0400

    Exercise 7

--- src/main/java/control_flow/DeliveryFee.java
+++ src/main/java/control_flow/DeliveryFee.java
@@ -1,4 +1,35 @@
 package control_flow;
+
+import java.util.Scanner;
+
 //Exercise 7: Implementing the else-if Statements
 public class DeliveryFee {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your distance?");
+        int distance = sc.nextInt();
+        sc.close();
+        int fee = deliveryFeeCalculator(distance);
+        System.out.println("Delivery Fee: $" + fee);
+
+    }
+
+    public static int deliveryFeeCalculator(int distance){
+
+        int fee = 0;
+        if (distance > 0 && distance < 5){
+            fee = 2;
+        } else if (distance >= 5 && distance < 15 ) {
+            fee = 5;
+        } else if (distance >= 15 && distance < 25){
+            fee = 10;
+        } else if (distance >= 25 && distance < 50) {
+            fee = 15;
+        }else {
+            fee = 20;
+        }
+        return fee;
+    }
+
 }
