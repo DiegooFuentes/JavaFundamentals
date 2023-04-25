5ad0c6a410229fb15829ae39601bb9236dcfbe30
DiegooFuentes <diegofuentesm123@gmail.com>
2023-04-24 23:59:39 -0400

    Exercise 6

--- src/main/java/control_flow/Booking.java
+++ src/main/java/control_flow/Booking.java
@@ -1,4 +1,15 @@
 package control_flow;
 //Exercise 6: Implementing a Simple if-else Statement
 public class Booking {
+    public static void main(String[] args) {
+
+        int seats = 3;
+        int req_ticket = 4;
+
+        if( (req_ticket == seats) || (req_ticket < seats)){
+            System.out.println("This booking can be accepted");
+        }else{
+            System.out.println("This booking is rejected");
+        }
+    }
 }
