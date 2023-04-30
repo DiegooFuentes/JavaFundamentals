fec6e0d18e201c7035f6b3cd023a4b125063df0e
DiegooFuentes <diegofuentesm123@gmail.com>
2023-04-30 08:14:59 -0400

    exercise 8

---
+++ src/main/java/control_flow/Looping.java
@@ -0,0 +1,15 @@
+package control_flow;
+//Exercise 8:Implementing a Simple for loop
+public class Looping {
+    public static void main(String[] args) {
+        System.out.println("Increasing order");
+        for(int i= 0 ; i <= 9 ; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("Decreasing order");
+        for(int k= 9; k >= 0; k--){
+            System.out.println(k);
+        }
+    }
+}
